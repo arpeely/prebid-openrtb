bc73969c06339233ec74bc5690283493c59e40f5
Or Yarnitzky <or.y@arpeely.com>
2024-06-05 17:51:52 +0300

    Support string content.context

--- adcom1/content_context.go
+++ adcom1/content_context.go
@@ -1,5 +1,7 @@
 package adcom1
 
+import "encoding/json"
+
 // ContentContext represents options for indicating the type of content being used or consumed by the user in which ads may appear.
 // This table has values derived from the TAG Inventory Quality Guidelines (IQG).
 type ContentContext int8
@@ -15,3 +17,22 @@ const (
 	ContentOther   ContentContext = 6 // 6 Other (i.e., none of the other categories applies)
 	ContentUnknown ContentContext = 7 // 7 Unknown
 )
+
+// Arpeely: sometimes, we recevieve ContentContext as a string
+// This fix is taken from bsm/openrtb
+
+func (n *ContentContext) UnmarshalJSON(data []byte) (err error) {
+	var v int
+
+	if len(data) > 2 && data[0] == '"' && data[len(data)-1] == '"' {
+		err = json.Unmarshal(data[1:len(data)-1], &v)
+	} else {
+		err = json.Unmarshal(data, &v)
+	}
+	if err != nil {
+		return err
+	}
+
+	*n = ContentContext(v)
+	return nil
+}
